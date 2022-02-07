100045406f80dab981dafbe7574d0d8fc46b6624
Snowin <20103104@hit.edu.in>
2022-02-08 01:52:05 +0530

    fixed the responsiveness problem in nav and hero section

--- js/index.js
+++ js/index.js
@@ -1,6 +1,13 @@
-const nav = document.querySelector("nav");
+const nav = document.querySelector(".primary-nav");
+const navToggle = document.querySelector(".nav-toggle");
 
-function toggle() {
-  nav.classList.toggle("display");
-  nav.classList.toggle("translate");
-}
+navToggle.addEventListener("click", () => {
+  const visiblity = nav.getAttribute("data-visible");
+  if (visiblity === "false") {
+    nav.setAttribute("data-visible", true);
+    navToggle.setAttribute("aria-expanded", true);
+  } else {
+    nav.setAttribute("data-visible", false);
+    navToggle.setAttribute("aria-expanded", false);
+  }
+});
