357ce861106c9c046b2c2666e25ea47ef82ab22c
Ruban Gino Singh <rubangino22@gmail.com>
2022-02-08 21:14:45 +0530

    Completed upto Team Section

--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
 const nav = document.querySelector(".primary-nav");
 const navToggle = document.querySelector(".nav-toggle");
 
+// NavBar Scroll Color Change
 window.addEventListener("scroll", () => {
   const primaryHeader = document.querySelector(".primary-header");
   primaryHeader.classList.toggle("sticky", window.scrollY > 0);
 });
 
+// Navbar Toggle Button
 function toggleNav() {
   const visiblity = nav.getAttribute("data-visible");
   if (visiblity === "false") {
