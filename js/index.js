33236d7e2cc43d469ff85fc0fe9e81e479aef3cb
Snowin <20103104@hit.edu.in>
2022-02-08 15:24:47 +0530

    nav, hero, about sections

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
 const nav = document.querySelector(".primary-nav");
 const navToggle = document.querySelector(".nav-toggle");
 
+window.addEventListener("scroll", () => {
+  const primaryHeader = document.querySelector(".primary-header");
+  primaryHeader.classList.toggle("sticky", window.scrollY > 0);
+});
+
 navToggle.addEventListener("click", () => {
   const visiblity = nav.getAttribute("data-visible");
   if (visiblity === "false") {
